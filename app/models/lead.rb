212829adc103b49b8b1d306487b23f467837be0c
andylinteau <andy.linteau@gmail.com>
2015-10-19 22:59:56 -0400

    add changes to production to enable mailer to work

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -10,7 +10,7 @@ class Lead < MailForm::Base
   def headers
     {
       :subject => "My Contact Form",
-      :to => "andy@banzaihq.com",
+      :to => "sales@banzaihq.com",
       :from => %("#{name}" <#{email}>) 
     }
   end
